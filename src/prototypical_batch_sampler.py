b37483501e89dae7585c57e71f5e4dd179518825
Inderway <1040214708@qq.com>
2021-02-23 17:38:51 +0800

    u

--- src/prototypical_batch_sampler.py
+++ src/prototypical_batch_sampler.py
@@ -30,6 +30,7 @@ class PrototypicalBatchSampler(object):
         self.iterations = iterations
 
         self.classes, self.counts = np.unique(self.labels, return_counts=True)
+        # 所有类初始成tensor
         self.classes = torch.LongTensor(self.classes)
 
         # create a matrix, indexes, of dim: classes X max(elements per class)
@@ -37,12 +38,22 @@ class PrototypicalBatchSampler(object):
         # for every class c, fill the relative row with the indices samples belonging to c
         # in numel_per_class we store the number of samples for each class/row
         self.idxs = range(len(self.labels))
+        # np.nan为一个标记值，意为not a number
+        # 创建一个m为类数，n为类众数的矩阵并设为nan
         self.indexes = np.empty((len(self.classes), max(self.counts)), dtype=int) * np.nan
+        # tensor化
         self.indexes = torch.Tensor(self.indexes)
         self.numel_per_class = torch.zeros_like(self.classes)
+        # labels为dataset.y
         for idx, label in enumerate(self.labels):
+            # argwhere返回满足条件的索引
+            # 获得dataset.y中的label在类数组中的索引
             label_idx = np.argwhere(self.classes == label).item()
+            # indexes的每一行代表一个类
+            # np.where输出满足条件的在数组indexes[label_idx]中的索引列表，第一个0获得索引列表，第二个0获得第一个满足条件的索引
+            # 在最前的nan值中填上该类在dataset.y中的索引
             self.indexes[label_idx, np.where(np.isnan(self.indexes[label_idx]))[0][0]] = idx
+            # 该类出现次数加1
             self.numel_per_class[label_idx] += 1
 
     def __iter__(self):
@@ -55,13 +66,19 @@ class PrototypicalBatchSampler(object):
         for it in range(self.iterations):
             batch_size = spc * cpi
             batch = torch.LongTensor(batch_size)
+            # randperm返回从0到类数的整数随机排列
             c_idxs = torch.randperm(len(self.classes))[:cpi]
             for i, c in enumerate(self.classes[c_idxs]):
+                # 切片, 起始为i*spc, 结束为(i+1)*spc-1
                 s = slice(i * spc, (i + 1) * spc)
                 # FIXME when torch.argwhere will exists
+                # arange生成一个从0到len-1的tensor并设为long型， 获得classes中c的索引
                 label_idx = torch.arange(len(self.classes)).long()[self.classes == c].item()
                 sample_idxs = torch.randperm(self.numel_per_class[label_idx])[:spc]
+                # 获得类c在indexes中保存的索引。如果spc为3，随机到的索引为2, 0, 3, 那么就取indexes[label_idx][2,0,3]里的值
+                # 该值为其对应的在dataset.y中的索引
                 batch[s] = self.indexes[label_idx][sample_idxs]
+            # 打乱小批的顺序
             batch = batch[torch.randperm(len(batch))]
             yield batch
 
---
+++ src/test_code.py
@@ -0,0 +1,4 @@
+import numpy as np
+s=slice(1,2)
+a=[1,2,3,4,5,6]
+print(a[s])
